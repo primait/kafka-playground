1ce92a37637aba3862ed9b74fe55c49ccb12ab1f
Matteo Busi <mbusi81@gmail.com>
2024-02-27 15:01:35 +0100

    Add mix to dialyzer PLT

--- elixir_avro/mix.exs
+++ elixir_avro/mix.exs
@@ -7,7 +7,8 @@ defmodule ElixirAvro.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
